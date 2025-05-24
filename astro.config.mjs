736fc222346f3e702fd895de1642d7e1602367d4
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-05-24 15:53:01 +0200

    node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+    session: true,  // habilita sesiones en el adaptador
+  }),
+  experimental: {
+    session: true, // habilita la feature experimental de sesiones
+  },
   integrations: [tailwind()],
 });
