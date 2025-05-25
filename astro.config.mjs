4e1da8ac96a918afc2c909fb22ab50b3e5aea1d4
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-05-25 20:06:06 +0200

    session borrado, astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
-
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
-    session: true,  // habilita sesiones en el adaptador
   }),
-  experimental: {
-    session: true, // habilita la feature experimental de sesiones
-  },
   integrations: [tailwind()],
 });
