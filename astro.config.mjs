1655bc4d444a78a0aea11b712ac0edccb8675e20
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-05-26 18:54:56 +0200

    comprobacion dist, pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  adapter: node({ mode: 'standalone' }),
   output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [tailwind()],
 });
