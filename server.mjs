53245c1f9734c5b0d3d4dffc453368a765763ba2
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-05-27 12:11:16 +0200

    comprobaciones server.mjs

--- server.mjs
+++ server.mjs
@@ -2,7 +2,8 @@
 import { startServer } from './dist/server/entry.mjs';
 
 const PORT = process.env.PORT || 4321;
-
+console.log('Llamando a startServer...');
 startServer({ port: PORT });
-
+console.log('startServer() fue llamado, ¿sigue vivo el proceso?');
 console.log(`http://localhost:${PORT}`);
+setInterval(() => {}, 1000 * 60 * 60);
