c8b498d60b7c6be89c3b2bd86291ae4ff6f1783b
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-06-06 16:01:40 +0200

    prueba, db version antigua

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,4 +1,6 @@
-import dotenv from 'dotenv';  // Importa dotenvAdd commentMore actions
+// src/lib/db.js
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';  // Importa dotenv
 
 
 export const db = mysql.createPool({
