429fa7ed34b673fc06238187bfb94719c99446e1
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-06-06 16:00:28 +0200

    prueba, db version antigua

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,21 +1,12 @@
-// src/lib/db.js
-import mysql from 'mysql2/promise';
-import 'dotenv/config';  // Importa dotenv
+import dotenv from 'dotenv';  // Importa dotenvAdd commentMore actions
 
-// variable de vite que indica si estamos en desarrollo
-const isDev = import.meta.env.DEV;
 
-const dbConfig = {
-  host: isDev ? 'localhost' : import.meta.env.DB_HOST,
-  user: isDev ? import.meta.env.DB_USER_DEV : import.meta.env.DB_USER,
-  password: isDev ? import.meta.env.DB_PASSWORD_DEV : import.meta.env.DB_PASSWORD,
-  database: import.meta.env.DB_DATABASE,
-  port: Number(import.meta.env.DB_PORT),
-  ssl: isDev ? false : { rejectUnauthorized: true },
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-};
-
-
-export const db = mysql.createPool(dbConfig);
+});
